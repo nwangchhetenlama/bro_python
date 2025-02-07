5c8d362c960225e094eb78088d83faf6f1510fe6
nwangchhetenlama <chhetenlama86@gmail.com>
2025-02-07 14:45:41 +0545

    while loop and compund calculator

---
+++ while.py
@@ -0,0 +1,55 @@
+
+#while=execute some code while some condition remain true
+
+"""num=int(input("enter num between 1 and 10: "))
+while num<1 or num>10:
+
+    print(f"the num is  wrong {num}")
+    num=int(input("enter num between 1 and 10: "))
+
+print(f"the num is {num}")"""
+
+"""food=input("what do you like:")
+while not food=="q":
+    print(f"you like {food}")
+    food=input("what do you like:")
+
+print("stopeed")"""
+
+
+#python compund interest calculator
+"""principle=0
+rate=0
+time=0
+while principle<=0:
+    principle=float(input("enter the principal:"))
+    if principle<=0:
+        print("enter the number greter")
+
+while rate<=0:
+    rate=float(input("enter the rate:"))
+    if rate<=0:
+        print("enter nicelyy")
+
+while time<=0:
+    time=float(input("enter the time:"))
+    if time<=0:
+        print("enter time")"""
+
+"""print(principle)
+print(rate)
+print(time)"""
+
+
+"""total=principle*pow((1+(rate/100)),time)
+print(f"the compund interest is {total}")
+"""
+"""principle=0
+while True:
+    principle=int(input("enter the principal:"))
+    if principle<=0:
+        principle=int(input("enter the principal:")) 
+    else:
+        break;
+
+print(principle)"""
